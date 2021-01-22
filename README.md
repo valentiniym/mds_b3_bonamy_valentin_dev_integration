799500abf755e93fbb796cd1c5d29be57abe2233
valentiniym <71429395+valentiniym@users.noreply.github.com>
2021-01-22 12:14:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tests_integration
+
+# Launch API
+
+node index.js
+
+# Launch test
+
+npm test 
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index')
+const should = chai.should()
+const expect = chai.expect
+
+chai.use(chaiHttp)
+
+// ---> DEBUT
+/**
+  * Génération des nouvelles couleurs et enregistrement de ces
+  * couleurs dans un tableau.
+  */
+const newValues = []
+const colorKey = 'NEW_COLOR_'
+let nextCursor = 0;
+const payloadColor = () => {
+  const nextColor = `${colorKey}${nextCursor}`
+  newValues.push(nextColor)
+  nextCursor++;
+  return { 'color': nextColor }
+}
+const getCurrentCulor = () => {
+  return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
+}
+// <-- FIN
